856108dc70f39f6760db486c07f381623bf4d2ac
Ju Dantas <jucienyds@gmail.com>
2021-05-13 18:26:17 -0300

    Ensinando fluxo em Java

---
+++ NomeClasse.java
@@ -0,0 +1,11 @@
+import com.projeto.*;
+
+public class NomeClasse {
+
+  public static void main (String args[]) {
+    Objeto obj = new Objeto();
+
+    System.out.println(obj.run());
+  }
+
+}
---
+++ com/projeto/Objeto.java
@@ -0,0 +1,7 @@
+package com.projeto;
+
+public class Objeto {
+  public String run(){
+    return "Rodou!";
+  }
+}
